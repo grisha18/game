062776347a1e7653a2a6f859772a4728ed3bee0f
grisha7777 <kurdjievgrigory777@gmail.com>
2021-01-15 20:50:51 +0300

    game

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {useState} from 'react';
+
+
+
+// сделать, чтобы внутри квадрата был или крестик или нолик
+// при нажатии чтобы переключало
+// прочитать https://habr.com/ru/post/429712/
+
+const Square = ()=>{
+const [color, setColor] = useState("red")
+
+  return(
+      <div style={{width: 100, height: 100, backgroundColor: color, display: 'flex', justifyContent: 'center', alignItems: 'center'}} onClick={()=>{setColor(color=="green" ? "red" : "green")}}>
+      </div>
+  )
+} 
+
+class Game extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      count: 0
+    }
+
+  }
+
+clickHandler = event => {
+  let data = event.target.getAttribute('data');
+  let currentSquares = this.state.squares;
+  console.log(currentSquares);
+  currentSquares[data] = 1;
+  currentSquares[data] = (this.state.count % 2 === 0) ? 'X' : 'O';
+  this.setState( {count: this.state.count + 1 });
+  this.setState({squares: currentSquares});  
+}
+
+
+render(){
+  return(
+    <div className="tic-tac-toe">
+        <div className="ttt-grid" onClick={this.clickHandler} data="0">
+        {this.state.squares[0]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="1">
+        {this.state.squares[1]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="2">
+        {this.state.squares[2]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="3">
+        {this.state.squares[3]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="4">
+        {this.state.squares[4]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="5">
+        {this.state.squares[5]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="6">
+        {this.state.squares[6]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="7">
+        {this.state.squares[7]}</div>
+        <div className="ttt-grid" onClick={this.clickHandler} data="8">
+        {this.state.squares[8]}</div>
+    </div>
+
+
+)
+}
+
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Square color="aqua" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default Game;
